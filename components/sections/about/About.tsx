1e6fa830a87e38d9cb88bf86665b0b6549bb8f48
CubeStorm <cubestorm72@gmail.com>
2022-01-21 22:14:19 +0100

    Added own image

--- components/sections/about/About.tsx
+++ components/sections/about/About.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Image from 'next/image';
 import { Container } from '@components/Container';
 import { Title } from '@components/sections/Title';
 
@@ -15,23 +16,14 @@ export const About: React.FC = () => {
 
                 <div className="flex flex-col lg:flex-row lg:items-center">
                     <div className="flex justify-center items-center mb-6 lg:w-1/2 lg:mb-0">
-                        <div className="flex justify-centeritems-center w-16 h-16 mr-5 rounded-full bg-indigo-50 sm:w-24 sm:h-24 xl:mr-10 xl:w-28 xl:h-28">
-                            <svg
-                                className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16 xl:w-20 xl:h-20"
-                                stroke="currentColor"
-                                viewBox="0 0 52 52"
-                            >
-                                <polygon
-                                    strokeWidth="3"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    fill="none"
-                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                                />
-                            </svg>
-                        </div>
+                        <Image
+                            src="/img/bee_in_enderchest.png"
+                            width={280}
+                            height={280}
+                            alt="Bee in enderchest"
+                        />
 
-                        <div className="">
+                        <div>
                             <h5 className="text-2xl font-extrabold sm:text-3xl xl:text-4xl text-text">
                                 9 312 435
                             </h5>
