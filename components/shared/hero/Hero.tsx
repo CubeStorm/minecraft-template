fed14a35ab1c984776718b6d89e6320b024120b9
CubeStorm <cubestorm72@gmail.com>
2022-01-21 22:14:42 +0100

    Lighter text

--- components/shared/hero/Hero.tsx
+++ components/shared/hero/Hero.tsx
@@ -60,7 +60,7 @@ export const Hero: React.FC = () => {
                             i baw się razem z nami
                         </h2>
 
-                        <p className="text-base text-gray-700 md:text-lg">
+                        <p className="text-base text-gray-500 md:text-lg">
                             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
                             quae. explicabo.
