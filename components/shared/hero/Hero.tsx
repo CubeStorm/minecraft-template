81abca8be37bbeb052e733b694b96f04a033246a
CubeStorm <cubestorm72@gmail.com>
2022-01-21 17:02:58 +0100

    Basic responsive hero

---
+++ components/shared/hero/Hero.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useLoader, useFrame } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Button } from '../Button';
+
+
+function Scene() {
+    const [scale, setScale] = React.useState(1.72);
+
+    const gltf = useLoader(GLTFLoader, '/img/crying_obsidian.gltf');
+
+    const meshRef = React.useRef(null);
+
+    useFrame(({ clock }) => {
+        if (meshRef.current) {
+            meshRef.current.rotation.z = clock.getElapsedTime() / 3;
+            meshRef.current.rotation.y = clock.getElapsedTime() / 4.5;
+            meshRef.current.rotation.x = clock.getElapsedTime() / 5.30;
+        }
+    });
+
+    const handleReScale = (scale: number) => setScale(scale);
+
+    return (
+        <primitive
+            ref={meshRef}
+            object={gltf.scene}
+            onPointerEnter={() => handleReScale(1.82)}
+            onPointerLeave={() => handleReScale(1.72)}
+            scale={scale}
+            rotation={[Math.PI / 1.54, 0, 2.0]}
+        />
+    )
+}
+
+export const Hero: React.FC = () => {
+    return (
+        <header className="sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto px-4 md:px-24 lg:px-8">
+            <div className="flex flex-col items-center justify-between lg:flex-row mt-6 lg:mt-0">
+                <div className="mb-3 lg:max-w-lg lg:pr-5 lg:mb-0">
+                    <div className="max-w-xl mb-6">
+                        <div>
+                            <p className="text-xs text-primary-200 font-bold tracking-wide mb-4 px-3 py-px">
+                                WERSJA: 1.18.2
+                            </p>
+                        </div>
+                        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-text sm:text-4xl sm:leading-none">
+                            Dołącz do nas
+                            <br className="hidden md:block" />
+                            i baw się razem z nami
+                        </h2>
+                        <p className="text-base text-gray-700 md:text-lg">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                            quae. explicabo.
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col items-center md:flex-row">
+                        <Button
+                            title="Skupiuj Adres IP"
+                            ariaLabel="Skopiuj adres ip serwera"
+                        />
+                    </div>
+                </div>
+
+                <div className="relative lg:w-1/2">
+                    {/* <Canvas>
+                        <Suspense fallback={null}>
+                            <ambientLight />
+                            <Scene />
+                        </Suspense>
+                    </Canvas> */}
+
+                    <img src="/img/render.png" alt="Render" className="relative z-0" />
+                </div>
+            </div>
+        </header>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Hero } from '@components/shared/hero/Hero';
 import type { NextPage } from 'next';
+
 
 const Home: NextPage = () => {
     return (
         <div className="w-full h-[2000px]">
-            <div className="text-gray-100">Hello World!</div>
+            <Hero />
         </div>
     )
 }
